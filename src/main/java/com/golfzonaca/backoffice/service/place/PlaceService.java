47b14f6a1fe3eff6a88318373f8e8c7bf72f1ff7
darkblose <darkblose@gmail.com>
2022-12-21 20:53:39 +0900

    update : coordinate

--- src/main/java/com/golfzonaca/backoffice/service/place/PlaceService.java
+++ src/main/java/com/golfzonaca/backoffice/service/place/PlaceService.java
@@ -163,7 +163,7 @@ public class PlaceService {
             throw new WrongAddressException("존재하지 않는 주소입니다.");
         }
         Map<String, Object> coordinateMap = elements.get(0);
-        Optional<Map<String, String>> optionalAddressMap = Optional.ofNullable((Map<String, String>) coordinateMap.get("roadAddressMap"));
+        Optional<Map<String, String>> optionalAddressMap = Optional.ofNullable((Map<String, String>) coordinateMap.get("road_address"));
         if (optionalAddressMap.isEmpty()) {
             coordinate.put("roadAddress", address);
             coordinate.put("longitude", (String) coordinateMap.get("x"));
